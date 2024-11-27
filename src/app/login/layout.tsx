2160cf64d26fce49d0f4a66dfa51735cf5ba50dd
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-27 23:36:53 +0500

    Auth Layout

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+};
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
