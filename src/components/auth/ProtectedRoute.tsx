144a9b864d55c77a1c625bde1cc66a0a29543097
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-27 23:17:37 +0500

    Loader

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -3,33 +3,38 @@ import { RootState } from "@/redux/store";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { DotLoader } from "react-spinners";
 
 interface ProtectedRouteProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-    const router = useRouter();
-    const [loading, setLoading] = useState(true);
-    const user = useSelector((state: RootState) => state.auth.user);
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const user = useSelector((state: RootState) => state.auth.user);
 
-    useEffect(() => {
-        const checkUser = () => {
-            if (user) {
-                setLoading(false);
-            } else {
-                router.push("/login");
-            }
-        };
+  useEffect(() => {
+    const checkUser = () => {
+      if (user) {
+        setLoading(false);
+      } else {
+        router.push("/login");
+      }
+    };
 
-        checkUser();
-    }, [user, router]);
+    checkUser();
+  }, [user, router]);
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <DotLoader size={100} color="#14b8a6" loading={loading} />
+      </div>
+    );
+  }
 
-    return user ? <>{children}</> : null;
+  return user ? <>{children}</> : null;
 };
 
 export default ProtectedRoute;
